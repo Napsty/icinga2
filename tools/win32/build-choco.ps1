dc6ae645e2dde31cc6c611bcf7b5d6e8832947c0
Noah Hilverling <noah.hilverling@icinga.com>
2020-05-26 11:41:39 +0200

    WIP

--- tools/win32/build-choco.ps1
+++ tools/win32/build-choco.ps1
@@ -8,35 +8,35 @@ if (-not (Test-Path env:ICINGA2_BUILDPATH)) {
   $env:ICINGA2_BUILDPATH = '.\build'
 }
 
-if(-not (Test-Path ".\icinga2\$($env:ICINGA2_BUILDPATH)\choco\chocolateyInstall.ps1.template")) {
+if(-not (Test-Path "$($env:ICINGA2_BUILDPATH)\choco\chocolateyInstall.ps1.template")) {
 	throw "Could not find Chocolatey install script template. Abort."
 }
 
-$chocoInstallScriptTemplatePath = ".\icinga2\$($env:ICINGA2_BUILDPATH)\choco\chocolateyInstall.ps1.template"
+$chocoInstallScriptTemplatePath = "$($env:ICINGA2_BUILDPATH)\choco\chocolateyInstall.ps1.template"
 $chocoInstallScript = Get-Content $chocoInstallScriptTemplatePath
 
-if(-not (Test-Path ".\*-x86.msi")) {
+if(-not (Test-Path "$($env:ICINGA2_BUILDPATH)\*-x86.msi")) {
 	throw "Could not find Icinga 2 32 bit MSI package. Abort."
 }
 
-$hashMSIpackage32 =  Get-FileHash ".\*-x86.msi"
+$hashMSIpackage32 =  Get-FileHash "$($env:ICINGA2_BUILDPATH)\*-x86.msi"
 Write-Output "File Hash for 32 bit MSI package: $($hashMSIpackage32.Hash)."
 
-if(-not (Test-Path ".\*-x86_64.msi")) {
+if(-not (Test-Path "$($env:ICINGA2_BUILDPATH)\*-x86_64.msi")) {
 	throw "Could not find Icinga 2 64 bit MSI package. Abort."
 }
 
-$hashMSIpackage64 =  Get-FileHash ".\*-x86_64.msi"
+$hashMSIpackage64 =  Get-FileHash "$($env:ICINGA2_BUILDPATH)\*-x86_64.msi"
 Write-Output "File Hash for 32 bit MSI package: $($hashMSIpackage64.Hash)"
 
 $chocoInstallScript = $chocoInstallScript.Replace("%CHOCO_32BIT_CHECKSUM%", "$($hashMSIpackage32.Hash)")
 $chocoInstallScript = $chocoInstallScript.Replace("%CHOCO_64BIT_CHECKSUM%", "$($hashMSIpackage64.Hash)")
 Write-Output $chocoInstallScript
 
-Set-Content -Path ".\icinga2\$($env:ICINGA2_BUILDPATH)\choco\chocolateyInstall.ps1" -Value $chocoInstallScript
+Set-Content -Path "$($env:ICINGA2_BUILDPATH)\choco\chocolateyInstall.ps1" -Value $chocoInstallScript
 
-cd ".\icinga2\$($env:ICINGA2_BUILDPATH)\choco"
+cd "$($env:ICINGA2_BUILDPATH)\choco"
 & "$($env:ProgramData)\chocolatey\choco.exe" "pack"
 cd "..\.."
 
-Move-Item -Path ".\icinga2\$($env:ICINGA2_BUILDPATH)\choco\*.nupkg" -Destination "$($env:ICINGA2_BUILDPATH)"
+Move-Item -Path "$($env:ICINGA2_BUILDPATH)\choco\*.nupkg" -Destination "$($env:ICINGA2_BUILDPATH)"
